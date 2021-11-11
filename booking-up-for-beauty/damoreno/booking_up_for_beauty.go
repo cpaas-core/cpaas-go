60dcf6668f6c4c35475143118e0e49505fc97e42
David Moreno García <damoreno@redhat.com>
2021-11-11 09:30:18 +0100

    Add solution for booking-up-for-beauty

---
+++ booking-up-for-beauty/damoreno/booking_up_for_beauty.go
@@ -0,0 +1,46 @@
+package booking
+
+import (
+    "fmt"
+    "time"
+)
+
+func parseDate(layout, date string) time.Time {
+    parsedDate, err := time.Parse(layout, date)
+
+    if err != nil {
+        fmt.Println("Oops:", err)
+    }
+
+    return parsedDate
+}
+
+// Schedule returns a time.Time from a string containing a date
+func Schedule(date string) time.Time {
+    return parseDate("1/02/2006 15:04:05", date)
+}
+
+// HasPassed returns whether a date has passed
+func HasPassed(date string) bool {
+    return parseDate("January 2, 2006 15:04:05", date).Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon
+func IsAfternoonAppointment(date string) bool {
+    hour := parseDate("Monday, January 2, 2006 15:04:05", date).Hour()
+
+    return hour >= 12 && hour < 18
+}
+
+// Description returns a formatted string of the appointment time
+func Description(date string) string {
+    parsedDate := parseDate("1/2/2006 15:04:05", date)
+
+    return fmt.Sprintf("You have an appointment on %s.",
+                       parsedDate.Format("Monday, January 2, 2006, at 15:04"))
+}
+
+// AnniversaryDate returns a Time with this year's anniversary
+func AnniversaryDate() time.Time {
+    return time.Date(time.Now().Year(), 9, 15, 0, 0, 0, 0, time.UTC)
+}
