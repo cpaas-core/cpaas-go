d77282e0af3e4737fe2cb2e5f2b4a0b2ef9bde53
Oleg Betsun <obetsun@obetsun.tlv.csb>
2021-11-18 12:10:16 +0200

    Booking_up_for_beauty_fixes

--- olegbetsun/booking_up_for_beauty.go
+++ olegbetsun/booking_up_for_beauty.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	)
 
-func ParseDate(layout string, date string) time.Time{
+	const parseLayout = "1/2/2006 15:04:05" 
+
+func parseDate(layout string, date string) time.Time{
 	t,err := time.Parse(layout, date)
 	if err != nil {
 		fmt.Printf("%v \n", err)
@@ -17,31 +19,27 @@ func ParseDate(layout string, date string) time.Time{
 
 // Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
-	layout := "1/02/2006 15:04:05"
-	t:= ParseDate(layout, date)
+	t:= parseDate(parseLayout, date)
 	return t
 }
 
 // HasPassed returns whether a date has passed
 func HasPassed(date string) bool {
-	layout := "January 2, 2006 15:04:05"
-	t := ParseDate(layout, date)
+	const layout = "January 2, 2006 15:04:05"
+	t := parseDate(layout, date)
 	return t.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-	layout := "Monday, January 2, 2006 15:04:05"
-	t:= ParseDate(layout, date)
-	noon := time.Date(t.Year(), t.Month(), t.Day(), 12, 0, 0, t.Nanosecond(), t.Location())
-	eve := time.Date(t.Year(), t.Month(), t.Day(), 17, 0, 0, t.Nanosecond(), t.Location())
-	return t.After(noon) && t.Before(eve)
+	const layout = "Monday, January 2, 2006 15:04:05"
+	t:= parseDate(layout, date)
+	return t.Hour()>=12 && t.Hour()<18
 }
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-	layout := "1/2/2006 15:04:05"
-	t:= ParseDate(layout, date)
+	t:= parseDate(parseLayout, date)
 	return "You have an appointment on "+t.Weekday().String()+", "+t.Month().String()+" "+strconv.Itoa(t.Day())+", "+strconv.Itoa(t.Year())+", at "+strconv.Itoa(t.Hour())+":"+strconv.Itoa(t.Minute())+"."
 
 }
