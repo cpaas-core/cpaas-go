963c980e135b53af34d30d342410a0160c916ef4
Hector Martinez <hemartin@redhat.com>
2021-11-19 10:16:11 +0100

    hemartin lasagna

---
+++ lasagna-master/hemartin/lasagna_master.go
@@ -0,0 +1,70 @@
+package lasagna
+
+const noodleQuantityPerLayer int = 50
+const sauceQuantityPerLayer float64 = 0.2
+const originalQuantityPortions float64 = 2.0
+
+func PreparationTime(layers []string, layerPrepTime int) int {
+	if layerPrepTime <= 0 {
+		layerPrepTime = 2
+	}
+	return layerPrepTime * len(layers)
+
+}
+
+func Quantities(layers []string) (int, float64) {
+	return QuantitiesFast(layers)
+}
+
+// This was the first implementation
+func QuantitiesSlow(layers []string) (int, float64) {
+	count := map[string]int{
+		"noodles": 0,
+		"sauce":   0,
+	}
+
+	for _, layer := range layers {
+		if layer == "noodles" || layer == "sauce" {
+			count[layer]++
+		}
+
+	}
+
+	return count["noodles"] * noodleQuantityPerLayer, float64(count["sauce"]) * sauceQuantityPerLayer
+}
+
+// Performant implementation
+func QuantitiesFast(layers []string) (int, float64) {
+	noodles := 0
+	sauce := 0.0
+
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodles += noodleQuantityPerLayer
+			continue
+		}
+
+		if layer == "sauce" {
+			sauce += sauceQuantityPerLayer
+			continue
+		}
+	}
+
+	return noodles, sauce
+}
+
+func AddSecretIngredient(friendList, myList []string) []string {
+	return append(myList, friendList[len(friendList)-1])
+}
+
+// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaledQuantities := make([]float64, len(quantities))
+	scaleFactor := float64(portions) / originalQuantityPortions
+
+	for index, quantity := range quantities {
+		scaledQuantities[index] = quantity * scaleFactor
+	}
+
+	return scaledQuantities
+}
