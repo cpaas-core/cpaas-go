367239db66522376405e1c22332afb9048f6103f
Pratiksha Patnaik <prpatnai@redhat.com>
2021-12-01 12:46:25 -0500

    lasagna master solution

--- lasagna-master/Pratster95/lasagna_master.go
+++ lasagna-master/Pratster95/lasagna_master.go
@@ -1,9 +1,39 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, time int) int {
+	if time != 0 {
+		return len(layers) * time
+	}
+	return len(layers) * 2
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	var noodles int = 0
+	var sauce float64 = 0.0
+	for i := 0; i < len(layers); i++ {
+		if layers[i] == "noodles" {
+			noodles += 50
+		}
+		if layers[i] == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList []string, myList []string) []string {
+	return append(myList, friendsList[len(friendsList)-1])
+}
 
 // TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	var amounts []float64
+	for _, v := range quantities {
+		amounts = append(amounts, v*float64(portions)/2.0)
+	}
+	return amounts
+
+}
