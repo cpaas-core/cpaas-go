02c7a7c4d64a8b20ca8c677bf908bf2410a1fbe8
Pratiksha Patnaik <pratikshapatnaik95@gmail.com>
2021-12-03 08:21:52 -0500

    Update lasagna_master.go

--- lasagna-master/Pratster95/lasagna_master.go
+++ lasagna-master/Pratster95/lasagna_master.go
@@ -1,39 +1,41 @@
 package lasagna
 
+
 // TODO: define the 'PreparationTime()' function
-func PreparationTime(layers []string, time int) int {
-	if time != 0 {
-		return len(layers) * time
-	}
-	return len(layers) * 2
+func PreparationTime(layers []string, time int) int{
+    if time != 0{
+        return len(layers) * time
+    }
+    return len(layers) * 2
 }
 
 // TODO: define the 'Quantities()' function
-func Quantities(layers []string) (int, float64) {
-	var noodles int = 0
-	var sauce float64 = 0.0
-	for i := 0; i < len(layers); i++ {
-		if layers[i] == "noodles" {
-			noodles += 50
-		}
-		if layers[i] == "sauce" {
-			sauce += 0.2
-		}
+func Quantities(layers []string) (int, float64){
+    noodles, sauce := 0, 0.0
+    for _, ingredient := range layers {
+		if ingredient == "noodles"{
+            noodles += 50
+        }
+    	if ingredient == "sauce"{
+            sauce += 0.2
+        }
 	}
-	return noodles, sauce
+    return noodles, sauce
 }
 
 // TODO: define the 'AddSecretIngredient()' function
-func AddSecretIngredient(friendsList []string, myList []string) []string {
-	return append(myList, friendsList[len(friendsList)-1])
+func AddSecretIngredient(friendsList []string, myList []string) []string{
+    return append(myList, friendsList[len(friendsList)-1])
 }
 
 // TODO: define the 'ScaleRecipe()' function
-func ScaleRecipe(quantities []float64, portions int) []float64 {
-	var amounts []float64
-	for _, v := range quantities {
-		amounts = append(amounts, v*float64(portions)/2.0)
-	}
+func ScaleRecipe(quantities []float64, portions int) []float64{
+   	var amounts []float64
+    share := float64(portions)/2.0
+    for _, quantity := range quantities {
+        amounts = append(amounts, quantity * share)
+    }
 	return amounts
-
+    
+    
 }
