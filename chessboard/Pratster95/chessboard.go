816155e604969f16a200806e7b635b71524f82ec
Pratiksha Patnaik <pratikshapatnaik95@gmail.com>
2021-12-09 10:39:27 -0500

    Update chessboard.go

--- chessboard/Pratster95/chessboard.go
+++ chessboard/Pratster95/chessboard.go
@@ -1,62 +1,50 @@
 package chessboard
 
 // Declare a type named Rank which stores if a square is occupied by a piece - this will be a slice of bools
-type Rank []bool
+type Rank [8]bool
 
 // Declare a type named Chessboard contains a map of eight Ranks, accessed with values from "A" to "H"
 type Chessboard map[string]Rank
 
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank
-func CountInRank(cb Chessboard, rank string) int {
-    count := 0
-    _, exists := cb[rank]
-    if exists == true{
-        for _, v := range cb[rank]{
-            if v == true{
-                count += 1
-            }
-        }
-    	
-    }
- 	return count
+func CountInRank(cb Chessboard, rank string) (count int) {
+	_, exists := cb[rank]
+	if exists == true {
+		for _, square := range cb[rank] {
+			if square == true {
+				count += 1
+			}
+		}
+
+	}
+	return
 
 }
 
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file
-func CountInFile(cb Chessboard, file int) int {
-	count := 0
-    if file >= 1 && file <=8{
-        for _, v := range cb {
-            if v[file - 1] == true {
-                count += 1
-            }
-        }
-    }
-	return count
+func CountInFile(cb Chessboard, file int) (count int) {
+	if file >= 1 && file <= 8 {
+		for _, rank := range cb {
+			if rank[file-1] == true {
+				count += 1
+			}
+		}
+	}
+	return
 }
 
 // CountAll should count how many squares are present in the chessboard
 func CountAll(cb Chessboard) int {
-	count := 0
-    for _, v := range cb {
-        for range v {
-            count ++
-        }
-    }
-	return count
+	return len(cb) * 8
+
 }
 
 // CountOccupied returns how many squares are occupied in the chessboard
-func CountOccupied(cb Chessboard) int {
-	count := 0
-    for _, v := range cb {
-        for _, v2:= range v {
-            if v2 == true {
-                count ++
-            }
-        }
-    }
-	return count
+func CountOccupied(cb Chessboard) (count int) {
+	for rank := range cb {
+		count += CountInRank(cb, rank)
+	}
+	return
 }
