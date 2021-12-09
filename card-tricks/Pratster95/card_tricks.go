34953268ba0c896344ec7e046d50aab6b8aa4e17
Pratiksha Patnaik <pratikshapatnaik95@gmail.com>
2021-12-09 10:13:44 -0500

    Added formatting

--- card-tricks/Pratster95/card_tricks.go
+++ card-tricks/Pratster95/card_tricks.go
@@ -3,37 +3,37 @@ package cards
 // GetItem retrieves an item from a slice at given position. The second return value indicates whether
 // the given index exists in the slice or not.
 func GetItem(slice []int, index int) (int, bool) {
-	if index < 0 || index > len(slice) - 1{
-        return 0, false
-    }
-	return slice[index],true
+	if index < 0 || index > len(slice)-1 {
+		return 0, false
+	}
+	return slice[index], true
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-    if index < 0 || index > len(slice) - 1{
-        slice = append(slice,value)
-    } 	else {
-    slice[index] = value
-    }
+	if index < 0 || index > len(slice)-1 {
+		slice = append(slice, value)
+	} else {
+		slice[index] = value
+	}
 	return slice
 }
 
 // PrefilledSlice creates a slice of given length and prefills it with the given value.
 func PrefilledSlice(value, length int) []int {
-    var slice []int 
-	for k := 0; k< length; k++{
-        slice = append(slice, value)
-    }
-    return slice
+	var slice []int
+	for k := 0; k < length; k++ {
+		slice = append(slice, value)
+	}
+	return slice
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
 
-	if index >=0 && index <= len(slice) - 1{
-		slice = append(slice[:index],slice[index+1:]...)
-        }
-        return slice  
+	if index >= 0 && index <= len(slice)-1 {
+		slice = append(slice[:index], slice[index+1:]...)
+	}
+	return slice
 }
