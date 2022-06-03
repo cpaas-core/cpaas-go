db66bddef0f89fdbfd07117d06e2d570cd4f8ef9
Samuel Munilla <smunilla@redhat.com>
2022-06-03 16:11:21 -0400

    Change solution to show adding methods to types

--- space-age/smunilla/space_age.go
+++ space-age/smunilla/space_age.go
@@ -1,25 +1,32 @@
 package space
 
+var NumSecondsInEarthYear float64 = 31557600.0
+
+var OrbitalPeriod = map[Planet]float64{
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1.0,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
 type Planet string
 
-var (
-	EarthSecondsInYear float64 = 31557600.0
-	OrbitalPeriods             = make(map[Planet]float64)
-)
+func (p *Planet) OrbitalPeriod() float64 {
+	return OrbitalPeriod[*p]
+}
 
-func init() {
-	OrbitalPeriods = map[Planet]float64{
-		"Mercury": 0.2408467,
-		"Venus":   0.61519726,
-		"Earth":   1.0,
-		"Mars":    1.8808158,
-		"Jupiter": 11.862615,
-		"Saturn":  29.447498,
-		"Uranus":  84.016846,
-		"Neptune": 164.79132,
-	}
+func (p *Planet) SecondsInAYear() float64 {
+	return p.OrbitalPeriod() * NumSecondsInEarthYear
+}
+
+func (p *Planet) HowOldAmI(seconds float64) float64 {
+	return seconds / p.SecondsInAYear()
 }
 
 func Age(seconds float64, planet Planet) float64 {
-	return seconds / EarthSecondsInYear / OrbitalPeriods[planet]
+	return planet.HowOldAmI(seconds)
 }
