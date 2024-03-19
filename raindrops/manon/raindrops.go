735da2856b70442dbeb85bcbfbf565147bbb3da3
Marta Anon <manon@redhat.com>
2024-03-19 19:05:26 +0100

    manon expenses solution

--- raindrops/manon/raindrops.go
+++ raindrops/manon/raindrops.go
@@ -2,16 +2,23 @@ package raindrops
 
 import "strconv"
 
+var Drops = map[int]string{
+	3: "Pling",
+	5: "Plang",
+	7: "Plong",
+}
+
 func Convert(number int) string {
 	var response string
-	if number%3 == 0 {
-		response += "Pling"
-	}
-	if number%5 == 0 {
-		response += "Plang"
-	}
-	if number%7 == 0 {
-		response += "Plong"
+	var potentialFactor = 1
+	for dropCounter := 1; dropCounter <= len(Drops); dropCounter += 0 {
+		if sound, exists := Drops[potentialFactor]; exists == true {
+			if number%potentialFactor == 0 {
+				response += sound
+			}
+			dropCounter++
+		}
+		potentialFactor++
 	}
 	if response == "" {
 		response = strconv.Itoa(number)
