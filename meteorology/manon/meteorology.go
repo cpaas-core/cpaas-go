c88ee41529a0578567b8778405eeb9d17cb4c825
Marta Anon <manon@redhat.com>
2024-03-19 11:16:57 +0100

    manon meteorology solution

---
+++ meteorology/manon/meteorology.go
@@ -0,0 +1,58 @@
+package meteorology
+
+import "fmt"
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+func (tu TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[tu]
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit)
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+func (su SpeedUnit) String() string {
+	units := []string{"km/h", "mph"}
+	return units[su]
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+func (s Speed) String() string {
+	return fmt.Sprintf("%d %s", s.magnitude, s.unit)
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+func (m MeteorologyData) String() string {
+	return fmt.Sprintf("%s: %s, Wind %s at %s, %d%% Humidity",
+		m.location, m.temperature, m.windDirection, m.windSpeed, m.humidity)
+}
